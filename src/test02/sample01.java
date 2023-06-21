94a92a373df48d463e91f3cdc089fe9470b26b8d
id <id@AQ-0131>
2023-06-21 16:43:13 +0900

    yyyのプッシュテスト

--- src/test02/sample01.java
+++ src/test02/sample01.java
@@ -8,7 +8,7 @@ public class sample01 {
 		System.out.print("ペアプログラミングのアイデアなんだけど、本郷さんが地域別の日本酒紹介サイトを作ったみたいで");
 		System.out.println("それの旅行版を作れたら面白そうだなって思った！");
 
-		System.out.println("吉岡だよ！");
+		System.out.println("吉岡です！");
 		System.out.println("なるほど～良いと思う！");
 		System.out.println("具体的にはどんな機能や画面が必要になりそう？");
 		
